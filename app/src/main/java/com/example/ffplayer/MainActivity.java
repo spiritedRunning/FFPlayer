66f09b4117e587a7290ac27ba5cc5ec8b2ef1cc5
zachliu <freedream0511@gmail.com>
2021-02-13 22:28:41 +0800

    处理横竖屏切换和锁屏问题

--- app/src/main/java/com/example/ffplayer/MainActivity.java
+++ app/src/main/java/com/example/ffplayer/MainActivity.java
@@ -1,28 +1,37 @@
 package com.example.ffplayer;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.widget.TextView;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
 
     private FFPlayer mPlayer;
+    private SurfaceView surfaceView;
+    private String path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        SurfaceView surfaceView = findViewById(R.id.surfaceView);
+        surfaceView = findViewById(R.id.surfaceView);
         surfaceView.getHolder().addCallback(this);
 
         mPlayer = new FFPlayer();
-//        mPlayer.setDataSource("/sdcard/bill.mp4");
-        mPlayer.setDataSource("http://ivi.bupt.edu.cn/hls/cctv1hd.m3u8");
+
+
+        path = "/sdcard/bill.mp4";
+//        path = "http://ivi.bupt.edu.cn/hls/cctv1hd.m3u8";
+        mPlayer.setDataSource(path);
         mPlayer.setOnPrepareListener(new FFPlayer.OnPrepareListener() {
             @Override
             public void onPrepared() {
@@ -31,6 +40,27 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
         });
 
 
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        surfaceView.getHolder().removeCallback(this);
+
+        setContentView(R.layout.activity_main);
+        surfaceView = findViewById(R.id.surfaceView);
+        surfaceView.getHolder().addCallback(this);
+        mPlayer.setDataSource(path);
+
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            ViewGroup.LayoutParams layoutParams = surfaceView.getLayoutParams();
+            layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT;
+            layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+            surfaceView.setLayoutParams(layoutParams);
+        } else {
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
     }
 
     @Override
